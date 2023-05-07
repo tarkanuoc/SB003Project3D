ee454392df08f9e1c388539c50c065c58d7e8332
tarkanuoc <35514613+tarkanuoc@users.noreply.github.com>
2023-05-07 17:22:40 +0700

    - Add Tilt left, right.

--- Assets/Scripts/HelicopterController.cs
+++ Assets/Scripts/HelicopterController.cs
@@ -3,13 +3,16 @@ using DG.Tweening;
 
 public class HelicopterController : MonoBehaviour
 {
+    [SerializeField] private Transform camera;
     public Rigidbody rigi;
-    float _targetRotationY;
     float _targetRotationX;
+    float _targetRotationY;
+    float _targetRotationZ;
+
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W)) 
+        if (Input.GetKey(KeyCode.W))
         {
             Forward();
         }
@@ -39,6 +42,11 @@ public class HelicopterController : MonoBehaviour
             Stop();
         }
 
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            StopForward();
+        }
+
         if (Input.GetKey(KeyCode.T))
         {
             Lean();
@@ -53,6 +61,22 @@ public class HelicopterController : MonoBehaviour
         {
             Landing();
         }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TiltLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TiltRight();
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            StopTilt();
+        }
+       // camera.localEulerAngles = new Vector3(4.3f, 0, 0);
     }
 
     void TakeOff()
@@ -72,16 +96,65 @@ public class HelicopterController : MonoBehaviour
         rigi.velocity = targetDirection.normalized * 10f;
     }
 
+    public void StopForward()
+    {
+        UnLean();
+        rigi.velocity = Vector3.zero;
+    }
+
+    void TiltLeft()
+    {
+        PitchLeft();
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.left;
+        rigi.velocity = targetDirection.normalized * 10f;
+    }
+
+    void TiltRight()
+    {
+        PitchRight();
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.left;
+        rigi.velocity = targetDirection.normalized * 10f;
+    }
+
+    void StopTilt()
+    {
+        StopPitch();
+        rigi.velocity = Vector3.zero;
+    }
+
+    void PitchLeft()
+    {
+        _targetRotationZ = 15f;
+        transform.DORotateQuaternion(Quaternion.Euler(_targetRotationX, _targetRotationY, _targetRotationZ), 1.5f);
+        camera.DORotateQuaternion(Quaternion.Euler(0, _targetRotationY, 0f), 1.5f);
+    }
+
+    void PitchRight()
+    {
+        _targetRotationZ = -15f;
+        transform.DORotateQuaternion(Quaternion.Euler(_targetRotationX, _targetRotationY, _targetRotationZ), 1.5f);
+        camera.DORotateQuaternion(Quaternion.Euler(0, _targetRotationY, 0f), 1.5f);
+    }
+
+    void StopPitch()
+    {
+        _targetRotationZ = 0f;
+        transform.DORotateQuaternion(Quaternion.Euler(_targetRotationX, _targetRotationY, _targetRotationZ), 1.5f);
+        camera.DORotateQuaternion(Quaternion.Euler(0, _targetRotationY, 0f), 1.5f);
+    }
+
     void Lean()
     {
         _targetRotationX = 15f;
         transform.DORotateQuaternion(Quaternion.Euler(_targetRotationX, _targetRotationY, 0f), 1.5f);
+       // camera.DORotateQuaternion(Quaternion.Euler(0, _targetRotationY, 0f), 1.5f);
     }
 
     void UnLean()
     {
         _targetRotationX = 0f;
         transform.DORotateQuaternion(Quaternion.Euler(_targetRotationX, _targetRotationY, 0f), 1.5f);
+     //   camera.DORotateQuaternion(Quaternion.Euler(0, _targetRotationY, 0f), 1.5f);
     }
 
     public void Stop()
