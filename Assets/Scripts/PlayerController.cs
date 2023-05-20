bbb6818eb5bdf3b5940a55ed7af7d595da197887
tarkanuoc <35514613+tarkanuoc@users.noreply.github.com>
2023-05-20 17:20:50 +0700

    + Add animation Walk, Run, Walk Back, Run Back

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,15 @@ public class PlayerController : MonoBehaviour
     float _targetRotationZ;
     public float speedWalk = 20f;
     private bool _isIdle;
+    private bool _isMoving;
     private float _preMousePosX;
     public float speedRotate = 5f;
     public float timeRotate = 0.5f;
+    private bool _isMoveBack = false;
+    private float _walkDirection;
+    private bool _isStopMoving;
+    private float _stopTime = 1.33f;
+    private float _VelocityStop;
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +32,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        if (_isStopMoving)
+        {
+            _stopTime -= Time.deltaTime;
+            _animator.SetFloat("StopSpeed", _stopTime / 1.33f);
+            rigi.velocity *= (_stopTime / 1.33f);
+
+            if (_stopTime <= 0f)
+            {
+                Debug.Log("======== stop");
+                _animator.SetTrigger("Idle");
+                rigi.velocity = Vector3.zero;
+                _isStopMoving = false;
+                _stopTime = 1.33f;
+                speedWalk = 20f;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
         {
             StopWalk();
         }
@@ -34,6 +57,37 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             Walk();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            WalkBack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _isMoving)
+        {
+            if (_isMoveBack)
+            {
+                _animator.SetFloat("MoveState", (float)MoveState.RunBack);
+            }
+            else
+            {
+                _animator.SetFloat("MoveState", (float)MoveState.Run);
+            }
+            speedWalk = 40f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) && _isMoving)
+        {
+            if (_isMoveBack)
+            {
+                _animator.SetFloat("MoveState", (float)MoveState.WalkBack);
+            }
+            else
+            {
+                _animator.SetFloat("MoveState", (float)MoveState.Walk);
+            }
+            speedWalk = 20f;
         }
 
         if (Input.GetMouseButton(1))
@@ -56,7 +110,11 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopWalk();
+        }
+
     }
 
     public void Walk()
@@ -64,14 +122,40 @@ public class PlayerController : MonoBehaviour
         Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.forward;
         rigi.velocity = targetDirection.normalized * speedWalk;
         _animator.SetTrigger("Walk");
+        _animator.SetFloat("MoveState", (float)MoveState.Walk);
         _animator.SetBool("IsMoving", true);
+        _isMoveBack = false;
+        _isMoving = true;
+        _isStopMoving = false;
+        _stopTime = 1.33f;
+    }
+
+    public void WalkBack()
+    {
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.forward * -1f;
+        rigi.velocity = targetDirection.normalized * speedWalk;
+        _animator.SetTrigger("Walk");
+        _animator.SetFloat("MoveState", (float)MoveState.WalkBack);
+        _animator.SetBool("IsMoving", true);
+        _isMoveBack = true;
+        _isMoving = true;
+        _isStopMoving = false;
+        _stopTime = 1.33f;
     }
 
     private void StopWalk()
     {
-        _animator.SetTrigger("Idle");
-        rigi.velocity = Vector3.zero;
+        //rigi.velocity = Vector3.zero;
+
+        _animator.SetFloat("MoveState", (float)MoveState.None);
+       
         _animator.SetBool("IsMoving", false);
+
+        _isMoveBack = false;
+        _isMoving = false;
+        _isStopMoving = true;
+        _VelocityStop = rigi.velocity.magnitude;
+        // speedWalk = 0f;
     }
 
     public void RotateLeft()
@@ -80,6 +164,10 @@ public class PlayerController : MonoBehaviour
         transform.DORotateQuaternion(Quaternion.Euler(0f, _targetRotationY, 0f), timeRotate);
 
         Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.forward;
+        if (_isMoveBack)
+        {
+            targetDirection *= -1f;
+        }
         rigi.velocity = targetDirection.normalized * rigi.velocity.magnitude;
     }
 
@@ -88,6 +176,22 @@ public class PlayerController : MonoBehaviour
         _targetRotationY += speedRotate;
         transform.DORotateQuaternion(Quaternion.Euler(0f, _targetRotationY, 0f), timeRotate);
         Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotationY, 0.0f) * Vector3.forward;
+
+        if (_isMoveBack)
+        {
+            targetDirection *= -1f;
+        }
+
         rigi.velocity = targetDirection.normalized * rigi.velocity.magnitude;
     }
+}
+
+public enum MoveState
+{
+    RunBack = -2,
+    WalkBack = -1,
+    None = 0,
+    Walk = 1,
+    Run = 2
+
 }
